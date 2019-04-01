dab0d0b386592ddc123746bd069926040fe2a2d5
qwert2603 <qwert2603@mail.ru>
2019-04-01 13:10:06 +0700

    items test

--- library/src/test/java/com/qwert2603/id_diff_util/qqqIdDiffUtilTest.kt
+++ library/src/test/java/com/qwert2603/id_diff_util/qqqIdDiffUtilTest.kt
@@ -1,6 +1,7 @@
 package com.qwert2603.id_diff_util
 
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListUpdateCallback
 import org.junit.Test
 
 class qqqIdDiffUtilTest {
@@ -9,37 +10,48 @@ class qqqIdDiffUtilTest {
 
     @Test
     fun `q q`() {
-        val old = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
-        val new = listOf(0, 3, 1, 2, 4, 5, 7, 8, 9, 6, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+        val old = listOf(
+            Item(1, "a"),
+            Item(2, "b"),
+            Item(3, "c"),
+            Item(4, "d")
+        )
+        val new = listOf(
+            Item(4, "d"),
+            Item(1, "a"),
+            Item(3, "c"),
+            Item(5, "e"),
+            Item(6, "f")
+        )
 
-        repeat(1000000) {
-            val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
-                override fun getOldListSize(): Int = old.size
-                override fun getNewListSize(): Int = new.size
-                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                    old[oldItemPosition] == new[newItemPosition]
+//        repeat(1000000) {
+        val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int = old.size
+            override fun getNewListSize(): Int = new.size
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                old[oldItemPosition].id == new[newItemPosition].id
 
-                override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                    old[oldItemPosition] == new[newItemPosition]
-            })
-        }
-//        diffResult.dispatchUpdatesTo(object : ListUpdateCallback {
-//            override fun onChanged(position: Int, count: Int, payload: Any?) {
-//                println("onChanged $position $count")
-//            }
-//
-//            override fun onMoved(fromPosition: Int, toPosition: Int) {
-//                println("onMoved $fromPosition $toPosition")
-//            }
-//
-//            override fun onInserted(position: Int, count: Int) {
-//                println("onInserted $position $count")
-//            }
-//
-//            override fun onRemoved(position: Int, count: Int) {
-//                println("onRemoved $position $count")
-//            }
-//        })
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                old[oldItemPosition] == new[newItemPosition]
+        })
+//        }
+        diffResult.dispatchUpdatesTo(object : ListUpdateCallback {
+            override fun onChanged(position: Int, count: Int, payload: Any?) {
+                println("onChanged $position $count")
+            }
+
+            override fun onMoved(fromPosition: Int, toPosition: Int) {
+                println("onMoved $fromPosition $toPosition")
+            }
+
+            override fun onInserted(position: Int, count: Int) {
+                println("onInserted $position $count")
+            }
+
+            override fun onRemoved(position: Int, count: Int) {
+                println("onRemoved $position $count")
+            }
+        })
     }
 
 
